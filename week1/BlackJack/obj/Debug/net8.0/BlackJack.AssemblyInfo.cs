0fc15aed14e4cb7ebef02cc0ae3a6396b0648197
Chike Egbuchulam <egbuchulamchike@gmail.com>
2024-07-11 17:07:32 -0400

    new class added

--- week1/BlackJack/obj/Debug/net8.0/BlackJack.AssemblyInfo.cs
+++ week1/BlackJack/obj/Debug/net8.0/BlackJack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1381bf5222ccbc4bbe6f98ce5285c7918c5dec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89636ccf5298c35ae4925680fbe9265ddd116ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackJack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
